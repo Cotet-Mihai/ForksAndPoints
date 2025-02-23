81183cf4f05decdc16d40fd274aea388b53031f7
Cotet Mihai <cotet.mihaita.cornel@gmail.com>
2025-02-23 12:40:39 +0200

    create navigation for rooms page

--- routes/rooms_routes.py
+++ routes/rooms_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, session, redirect, url_for
+from flask import Blueprint, request, session, redirect, url_for, render_template
 from model.Users import PersonalInformations, Users
 from app import db
 
@@ -9,15 +9,13 @@ def rooms():
     if 'userID' not in session:
         return redirect(url_for('auth.sign_in'))
 
-    return redirect(url_for('in_progress.in_progress'))
+    return render_template('rooms/rooms.html')
 
 @rooms_bp.route('/get-questionnaire-info', methods = ['POST'])
 def get_questionnaire_info():
     data = request.get_json()
 
     try:
-
-        print(data)
         set_personal_information = PersonalInformations(
             userID = session['userID'],
             gender = data['gender'],
@@ -43,6 +41,12 @@ def get_questionnaire_info():
     except Exception as e:
         print(e)
 
-
-
     return '/rooms'
+
+
+@rooms_bp.route('/find-room')
+def find_room():
+    if 'userID' not in session:
+        return redirect(url_for('auth.sign_in'))
+
+    return render_template('rooms/find_room.html')
